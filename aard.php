54abc0d7b91f5455ee063bf74641fb19ba9f6e9d
FryerTuck <kindhacker@protonmail.com>
2019-11-08 14:13:25 +0200

    saved '/lekrtest/aard.php'

--- aard.php
+++ aard.php
@@ -5,6 +5,7 @@ class lekrtest
 {
     static function __init()
     {
-        permit::clan("work");
+        permit::clan("work"); $stem="/lekrtest";
+        if(NAVIPATH===$stem){$indx=path::indx($stem); ekko::path("$stem/$indx");};
     }
 }
