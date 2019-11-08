4b85c892cc5e7f503366aeb4bf8fdd9864274924
FryerTuck <kindhacker@protonmail.com>
2019-11-08 14:18:32 +0200

    saved '/lekrtest/aard.php'

--- aard.php
+++ aard.php
@@ -6,7 +6,7 @@ class lekrtest
     static function __init()
     {
         permit::clan("work"); $stem="/lekrtest";
-        todo::{"Anon stem boot"}("make stem classes deault to alternative path::indx");
+        todo::{"Anon stem boot"}("make stem classes default to alternative path::indx");
         if(NAVIPATH==="$stem/"){ekko::path("$stem/README.md");};
     }
 }
