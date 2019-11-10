69ee9a53c0265e47bd839eeb24740fc2541283fc
FryerTuck <kindhacker@protonmail.com>
2019-11-10 18:02:16 +0200

    saved '/lekrtest/dist/script.js'

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,21 @@
-(function()
-{
-    alert("olo");
-}());
+"use strict";
+
+
+// defn :: (contants) : shortands to compliment DRY coding principle
+// ----------------------------------------------------------------------------------------------------------------------------
+    const MAIN = window;
+    const VOID = (function(){}()); // undefined
+// ----------------------------------------------------------------------------------------------------------------------------
+
+
+
+// tool :: bake : define hardened properties
+// ----------------------------------------------------------------------------------------------------------------------------
+    const bake = function(o,k,v)
+    {
+        if(!o||!o.hasOwnProperty){return}; if(v==VOID){v=o[k]};
+        let c={enumerable:false,configurable:false,writable:false,value:v};
+        let r=true; try{Object.defineProperty(o,k,c);}catch(e){r=false};
+        return r;
+    };
+// ----------------------------------------------------------------------------------------------------------------------------
