16220ddc974d0363add96916987edbd6f4394071
FryerTuck <kindhacker@protonmail.com>
2019-11-11 08:32:42 +0200

    saved '/lekrtest/dist/index.php'

--- dist/index.php
+++ dist/index.php
@@ -63,7 +63,7 @@ $html = ob_get_clean();
         $what = $_POST['find'];
         $html = spuf("https://www.lekkeslaap.co.za/akkommodasie/$what");
         $html = explode("<div class='swiper-wrapper' ",$html)[1]; $html=explode('      ',$html)[1];
-        $html = str_replace('<div class="swiper-lazy-preloader"></div>','',$html);
+        $html = str_replace('<div class="swiper-lazy-preloader"></div>','',trim($html));
         die($html);
         $list = [];
         $json = json_encode($list);
