4a75c4a09e949373d7f6dc3703e3ab9f0f169918
FryerTuck <kindhacker@protonmail.com>
2019-11-11 15:03:28 +0200

    saved '/lekrtest/dist/script.js'

--- dist/script.js
+++ dist/script.js
@@ -36,7 +36,7 @@
     bake(HTMLDivElement.prototype,"preslide",function(a)
     {
 dump(a);
-//         this.style.display="none"; this.list=a; this.indx=0; this.innerHTML="";
+        this.style.display="none"; this.list=a; this.indx=0; this.innerHTML="";
         
 //         if(!this.touched)
 //         {
