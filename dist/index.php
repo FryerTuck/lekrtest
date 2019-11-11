a418af91aab73e1f65dac063911a1b592290eaf8
FryerTuck <kindhacker@protonmail.com>
2019-11-11 05:15:52 +0200

    saved '/lekrtest/dist/index.php'

--- dist/index.php
+++ dist/index.php
@@ -1,15 +1,20 @@
 <?php
 
-    function seek($n)
+    function wget($uri,$uas=null,$ref=null)
     {
-        $data = [];
-        return $data;
-    }
+        if(!is_string($uri)){return;}; if(strpos($uri,'http')===false){return;}; if(!isee('curl')){return;}; $ipa=envi('USERADDR');
+        if(!$uas){$uas=envi('USER_AGENT');}; if(!$ref){$ref=envi('REFERER'); if(!$ref){$ref='http://example.com/index.html';}};
+        $o=array(CURLOPT_RETURNTRANSFER=>1,CURLOPT_SSL_VERIFYPEER=>false,CURLOPT_URL=>$uri,CURLOPT_USERAGENT=>$uas,CURLOPT_REFERER=>$ref);
+        $c=curl_init(); curl_setopt_array($c,$o); curl_setopt($c,CURLOPT_HTTPHEADER,array("REMOTE_ADDR: $ipa", "HTTP_X_FORWARDED_FOR: $ipa"));
+        $r=curl_exec($c); $e=null; if(!$r){$x=curl_error($c); if($x){$e=$x;};}; curl_close($c);
+        if($e){return "FAIL :: $e";}; return $r;
+    };
 
 
     if(isset($_POST['find']))
     {
-        $json = json_encode(seek($_POST['find']));
+        $what = $_POST['find'];
+        $json = json_encode(wget("https://www.lekkeslaap.co.za/akkommodasie/$what"));
         die($json);
     };
 
