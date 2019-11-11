08644228e807cb7a5c46d7c91adf0da5cc04db2d
FryerTuck <kindhacker@protonmail.com>
2019-11-11 06:41:11 +0200

    saved '/lekrtest/dist/script.js'

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,41 @@
 "use strict";
 
-    alert('oloz');
+// cond :: (tools) : only define some short-hand tools if missing
+// ----------------------------------------------------------------------------------------------------------------------------
+    if((typeof dump)=="undeined")
+    {
+        const MAIN = window; // super-global
+        const VOID = (function(){}()); // undefined
+    
+        const bake = function(o,k,v)
+        {
+            if(!o||!o.hasOwnProperty){return}; if(v==VOID){v=o[k]};
+            let c={enumerable:false,configurable:false,writable:false,value:v};
+            let r=true; try{Object.defineProperty(o,k,c);}catch(e){r=false};
+            return r;
+        };
+    
+        const list = function(a)
+        {return ([].slice.call(a))};
+    
+        const dump = function()
+        {console.log.apply(console,list(arguments))};
+    };
+// ----------------------------------------------------------------------------------------------------------------------------
 
+
+
+// tool :: (imageSlide) : proto-method that mutate div-nodes into an image sliders
+// ----------------------------------------------------------------------------------------------------------------------------
+    bake(HTMLDivElement.prototype,"imageSlide",function()
+    {
+        this.args = list(arguments);
+        dump(this.args);
+    });
+// ----------------------------------------------------------------------------------------------------------------------------
+
+
+// init
+// ----------------------------------------------------------------------------------------------------------------------------
+    alert('oloz');
+// ----------------------------------------------------------------------------------------------------------------------------
