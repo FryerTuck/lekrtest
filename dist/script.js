c2abc51bedc4de0e387c22f9fb7ee528bb552839
FryerTuck <kindhacker@protonmail.com>
2019-11-12 01:58:31 +0200

    saved '/lekrtest/dist/script.js'

--- dist/script.js
+++ dist/script.js
@@ -88,7 +88,7 @@
         purl("/lekrtest/dist/",{find:n},(resp)=>
         {
             resp=resp.body; if(!resp.startsWith("[")||!resp.endsWith("]")||(resp=="[]")){return};
-            document.getElementById("card").preslide(JSON.parse(resp));
+            document.getElementById("wrap").preslide(JSON.parse(resp));
         });
     };
 // ----------------------------------------------------------------------------------------------------------------------------
