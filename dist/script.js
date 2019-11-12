928830d4ed959ea1221c6fbc7cd31a268424a26a
FryerTuck <kindhacker@protonmail.com>
2019-11-12 07:06:43 +0200

    saved '/lekrtest/dist/script.js'

--- dist/script.js
+++ dist/script.js
@@ -85,6 +85,7 @@
     {
         document.getElementById("card").style.opacity=0;
         document.getElementById("busy").style.display='inline-block';
+        document.getElementById("labl").innerHTML=n.split('-').join(' ');
 
         purl("/lekrtest/dist/",{find:n},(resp)=>
         {
