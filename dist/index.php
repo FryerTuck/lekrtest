08146aa63f06c55884c97e7a75dcda05b6c230ea
FryerTuck <kindhacker@protonmail.com>
2019-11-12 08:04:35 +0200

    saved '/lekrtest/dist/index.php'

--- dist/index.php
+++ dist/index.php
@@ -15,8 +15,7 @@
                 <div id="info" class="center">
                     <p id="text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                        ex ea commodo consequat.
+                        dolore magna aliqua.
                     </p>
                     <div id="rate">rating</div> <div id="cost">costing</div>
                 </div>
